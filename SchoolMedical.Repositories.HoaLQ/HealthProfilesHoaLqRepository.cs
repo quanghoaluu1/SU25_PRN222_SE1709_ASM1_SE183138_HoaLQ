f75f9ff202e794786249d1c6dd23d4a98ae9449d
quanghoaluu1 <hoalqse183138@fpt.edu.vn>
2025-05-15 17:37:04 +0700

    Add HealthProfilesHoaLqRepository and vcs.xml configuration

---
+++ SchoolMedical.Repositories.HoaLQ/HealthProfilesHoaLqRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SchoolMedical.Repositories.HoaLQ.Basic;
+using SchoolMedical.Repositories.HoaLQ.Models;
+
+namespace SchoolMedical.Repositories.HoaLQ
+{
+    public class HealthProfilesHoaLqRepository: GenericRepository<HealthProfilesHoaLq>
+    {
+        public HealthProfilesHoaLqRepository()
+        {
+            _context ??= new SU25_PRN222_SE1709_G1_SchoolMedicalContext();
+        }
+
+        public HealthProfilesHoaLqRepository(SU25_PRN222_SE1709_G1_SchoolMedicalContext context)
+        {
+            _context = context;
+        }
+        
+        public new async Task< List<HealthProfilesHoaLq>> GetAllAsync()
+        {
+            return await _context.HealthProfilesHoaLqs
+                .Include(h => h.Student)
+                .ToListAsync();
+        }
+    }
+}
